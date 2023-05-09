6b58178bfe91188d2f66832da0044838181d2517
Dimash <dbek1405@gmail.com>
2023-05-09 16:45:56 +0600

    Schedule Page

--- Enactus/Presentation Layer/Tabbar/View/Modules/Schedule/Controller/ScheduleViewController.swift
+++ Enactus/Presentation Layer/Tabbar/View/Modules/Schedule/Controller/ScheduleViewController.swift
@@ -1,8 +1,81 @@
 import UIKit
+import FSCalendar
 
 final class ScheduleViewController: UIViewController {
+    
+    var calendarHeightConstraint : NSLayoutConstraint?
+    
+    private lazy var calendar: FSCalendar = {
+        let calendar = FSCalendar()
+        calendar.delegate = self
+        calendar.dataSource = self
+        calendar.setScope(.week, animated: true)
+        return calendar
+    }()
+    
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.layer.cornerRadius = 8
+        tableView.separatorStyle = .none
+        tableView.regiter(cellClass: ScheduleTableViewCell.self)
+        return tableView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        setup()
+        title = "Schedule"
     }
+    
+    private func setup() {
+        setupSubviews()
+        setupConstraints()
+    }
+    private func setupSubviews() {
+        [calendar, tableView].forEach{
+            view.addSubview($0)
+        }
+    }
+    private func setupConstraints() {
+        calendarHeightConstraint = NSLayoutConstraint(item: calendar, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 300)
+        calendar.addConstraint(calendarHeightConstraint ?? NSLayoutConstraint())
+        calendar.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide)
+            make.left.right.equalToSuperview().inset(24)
+        }
+        tableView.snp.makeConstraints { make in
+            make.top.equalTo(calendar.snp.bottom).offset(16)
+            make.left.right.equalToSuperview().inset(24)
+            make.bottom.equalToSuperview()
+        }
+    }
+}
+
+extension ScheduleViewController: FSCalendarDelegate, FSCalendarDataSource {
+    func calendar(_ calendar: FSCalendar, boundingRectWillChange bounds: CGRect, animated: Bool) {
+        calendarHeightConstraint?.constant = bounds.height
+        view.layoutIfNeeded()
+    }
+    func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
+        print(date)
+    }
+}
+
+extension ScheduleViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = ScheduleTableViewCell()
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 92
+    }
+    
 }
