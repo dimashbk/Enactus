ab7672ae5be3593f0910c2c91f85020a5324454f
Dimash <dbek1405@gmail.com>
2023-05-12 17:16:42 +0600

    Refresh token

--- Enactus/NetworkLayer/AuthorizationService/Authorization Service.swift
+++ Enactus/NetworkLayer/AuthorizationService/Authorization Service.swift
@@ -22,7 +22,19 @@ final class AuthorizationService {
     
     var authorizationPath: AuthorizationPath = .none
     
-    var accessToken = String()
+    var accessToken: String {
+        get{
+            if UserDefaults.standard.value(forKey: "accessToken") == nil {
+                return ""
+            } else {
+                return  UserDefaults.standard.value(forKey: "accessToken") as! String
+            }
+        
+        }
+        set{
+                UserDefaults.standard.set(newValue, forKey: "accessToken")
+        }
+    }
     
     var correctCode = String()
     
@@ -34,25 +46,43 @@ final class AuthorizationService {
         networkService.postLogin(param: authorizationModel) { [weak self] result in
             self?.accessToken = result?.token.original.accessToken ?? ""
             self?.getProfileInfo()
-            print(self?.accessToken)
+
         }
     }
     
     func sendCode() {
         networkService.sendCode(param: authorizationModel.email) { [weak self] result in
             self?.correctCode = result?.code.code ?? ""
-            print(result)
             print(self?.correctCode)
         }
     }
     func resetPassword() {
         networkService.resetPassword(param: authorizationModel) { [weak self] result in
-            print(result)
+
         }
     }
     func register() {
         networkService.postRegister(param: authorizationModel) { [weak self] result in
-            print(result)
+
+        }
+    }
+    func refreshToken() {
+        guard let url = URL(string: "http://studc-api.kz/api/auth/refresh-token") else { return }
+        
+        var token = AuthorizationService.shared.accessToken
+        
+        let headers = ["Authorization": "Bearer \(token)"]
+        
+        print(token)
+        
+        networkService.sendRequest(url: url, method: "GET", headers: headers, body: nil) { (result: Result<Login,Error>) in
+            switch result {
+            case .success(let data):
+                print(data.message)
+                print(token)
+            case .failure(let error):
+                print("Error:", error.localizedDescription)
+            }
         }
     }
     func getProfileInfo() {
